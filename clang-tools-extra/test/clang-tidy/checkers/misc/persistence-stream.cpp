b5f97650538baffa42ca185f11e34eac858c0707
Dora Fekete <feketedora92@gmail.com>
2024-04-01 18:48:18 +0200

    parameter match

--- clang-tools-extra/test/clang-tidy/checkers/misc/persistence-stream.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/misc/persistence-stream.cpp
@@ -7,15 +7,19 @@ class MyStream : public std::fstream {};
 
 class MyModel
 {
+	void write(std::fstream f){}
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: found file stream parameter outside persistence class [misc-persistence-stream]
+	void test(int a, bool b) {}
+
 	int mFile;
-// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: found file field outside persistence class [misc-persistence-stream]
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: found file stream field outside persistence class [misc-persistence-stream]
 	bool mDummy;
 	char file2;
-// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: found file field outside persistence class [misc-persistence-stream]
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: found file stream field outside persistence class [misc-persistence-stream]
 	std::fstream thisisspecial;
-// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: found file field outside persistence class [misc-persistence-stream]
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: found file stream field outside persistence class [misc-persistence-stream]
 	MyStream y;
-// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: found file field outside persistence class [misc-persistence-stream]
+// CHECK-MESSAGES: :[[@LINE-1]]:5: warning: found file stream field outside persistence class [misc-persistence-stream]
 };
 
 class MyPersistence {};
